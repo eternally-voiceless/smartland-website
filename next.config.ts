8a7e4ec558aa9d59718c5549165e84b84f4e7a8f
eternally-voiceless <v.v.a.onside@gmail.com>
2024-12-27 12:17:39 +0500

    next.config.ts modified

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,11 @@
-///** @type {import('next').NextConfig} */
-//const nextConfig = {
-//  output: 'export',
-//  images: {
-//    unoptimized: true,
-//  },
-//  basePath: '/smartland-website',
-//  assetPrefix: '/smartland-website/',
-//}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/smartland-website',
+  assetPrefix: '/smartland-website/',
+}
 
-<<<<<<< HEAD
-//module.exports = nextConfig
-
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
-
-export default nextConfig;
-=======
 module.exports = nextConfig
->>>>>>> c9d87c33fe7e1ccbed34434288e18d315218ffe5
